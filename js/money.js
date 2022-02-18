6035263684ae2ac8e06093a714750c5c8a7563d9
shahmojno <mojno12@gmail.com>
2022-02-18 21:59:02 +0600

    7 commit

--- js/money.js
+++ js/money.js
@@ -1,61 +1,25 @@
 
 
 function getInputValue(inputValue) {
-    const inputFieldFood = document.getElementById(inputValue);
-    const foodAmount = inputFieldFood.value;
-    const foodAmountNumber = parseFloat(foodAmount);
-    return foodAmountNumber;
+    const inputField = document.getElementById(inputValue);
+    const valueAmount = inputField.value;
+    const amountNumber = parseFloat(valueAmount);
+    return amountNumber;
 }
 
 
 
 document.getElementById('calculate').addEventListener('click', function () {
-
     const incomeAmountNumber = getInputValue('income-money');
-
-    const notNumber = document.getElementById('notify-rent');
-    const successMessage = document.getElementById('notify-income');
-
-    if (incomeAmountNumber == -1) {
-        notNumber.style.display = 'block';
-    }
-    else if (typeof incomeAmountNumber != Number) {
-        successMessage.style.display = 'block';
-
-    }
-
-
-    else {
-        successMessage.style.display = 'none';
-    }
-
     const foodAmountNumber = getInputValue('food-money');
-
-
     const rentAmountNumber = getInputValue('rent-money');
-
-
-
-
-    // const inputFieldClothes = document.getElementById('clothes-money');
-    // const clothesAmount = inputFieldClothes.value;
-    // const clothesAmountNumber = parseFloat(clothesAmount);
     const clothesAmountNumber = getInputValue('clothes-money')
-
     const totalExpenses = foodAmountNumber + rentAmountNumber + clothesAmountNumber;
-
-
     const totalBalance = incomeAmountNumber - totalExpenses;
-
     document.getElementById('total-expenses').innerText = totalExpenses;
-
     document.getElementById('balance').innerText = totalBalance;
 
-
-
 });
-
-
 
 
 
@@ -81,4 +45,43 @@ document.getElementById('save-btn').addEventListener('click', function () {
 
 
 
+// function ErrorMassege() {
+//     const incomeAmountNumber = document.getElementById('income-money').value;
 
+//     const foodAmountNumber = document.getElementById('food-money').value;
+//     const successMessage = document.getElementById('notify-income');
+//     const failError = document.getElementById('notify-rent');
+//     if (incomeAmountNumber == foodAmountNumber) {
+
+//         successMessage.style.display = 'block';
+//         failError.style.display = 'none';
+//     }
+//     else {
+
+//         failError.style.display = 'block';
+//         successMessage.style.display = 'none';
+//     }
+// }
+
+
+
+
+document.getElementById('calculate').addEventListener('click', function () {
+    const incomeAmountNumber = document.getElementById('income-money').value;
+
+    const foodAmountNumber = document.getElementById('food-money').value;
+    const successMessage = document.getElementById('notify-income');
+    const failError = document.getElementById('notify-rent');
+    if (incomeAmountNumber == foodAmountNumber) {
+
+        successMessage.style.display = 'block';
+        failError.style.display = 'none';
+    }
+    else {
+
+        failError.style.display = 'block';
+        successMessage.style.display = 'none';
+    }
+}
+
+});
